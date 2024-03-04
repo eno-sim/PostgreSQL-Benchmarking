a0d1400db6eb4f2ad6250986358c8821ae777cd4
eno-sim <simonecapp6660@gmail.com>
2024-03-04 10:51:46 +0100

    update

--- SQL-files/indices.sql
+++ SQL-files/indices.sql
@@ -1,7 +1,6 @@
 CREATE INDEX idx_customer_cname ON customer(c_name);
 CREATE INDEX idx_lineitem_lorderkey ON lineitem(l_orderkey);
 CREATE INDEX idx_lineitem_lpartkey ON lineitem(l_partkey);
-CREATE INDEX idx_nation_nname ON nation(n_name);
 CREATE INDEX idx_orders_ccustkey ON orders(o_custkey);
 CREATE INDEX idx_part_ptype ON part(p_type);
 CREATE INDEX idx_supplier_snationkey ON supplier(s_nationkey);
