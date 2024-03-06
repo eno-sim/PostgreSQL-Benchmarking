7b2a1a201bb3a4c0d921f52d1dec7c0d82d8f613
eno-sim <simonecapp6660@gmail.com>
2024-03-06 13:45:28 +0100

    mv2 and indexes results

---
+++ SQL-files/indices-mv2.sql
@@ -0,0 +1,21 @@
+--Q1 NONE IS USEFUL--
+CREATE INDEX idx_n_name ON nation(n_name);
+CREATE INDEX idx_p_type ON part(p_type);
+CREATE INDEX idx_exp_nation ON mv_q1(export_nation);
+CREATE INDEX idx_imp_nation ON mv_q1(import_nation);
+CREATE INDEX idx_exp_region ON mv_q1(export_region);
+CREATE INDEX idx_imp_region ON mv_q1(import_region);
+
+DROP INDEX if exists idx_n_name;
+DROP INDEX if exists idx_p_type;
+DROP INDEX if exists idx_exp_nation;
+DROP INDEX if exists idx_imp_nation;
+DROP INDEX if exists idx_exp_region;
+DROP INDEX if exists idx_imp_region;
+
+
+
+
+
+--Q3--
+CREATE INDEX idx_c_name ON mv_q3(c_name);
