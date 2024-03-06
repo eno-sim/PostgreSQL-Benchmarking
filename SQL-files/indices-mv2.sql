88308182c0e8392463b6da3bb23c03f55837956d
eno-sim <simonecapp6660@gmail.com>
2024-03-06 17:00:16 +0100

    update

--- SQL-files/indices-mv2.sql
+++ SQL-files/indices-mv2.sql
@@ -1,14 +1,14 @@
 --Q1 NONE IS USEFUL--
 CREATE INDEX idx_n_name ON nation(n_name);
-CREATE INDEX idx_p_type ON part(p_type);
+CREATE INDEX idx_ptype ON mv_q1(ptype);
 CREATE INDEX idx_exp_nation ON mv_q1(export_nation);
 CREATE INDEX idx_imp_nation ON mv_q1(import_nation);
 CREATE INDEX idx_exp_region ON mv_q1(export_region);
 CREATE INDEX idx_imp_region ON mv_q1(import_region);
 
 DROP INDEX if exists idx_n_name;
-DROP INDEX if exists idx_p_type;
-DROP INDEX if exists idx_exp_nation;
+--DROP INDEX if exists idx_ptype;
+--DROP INDEX if exists idx_exp_nation;
 DROP INDEX if exists idx_imp_nation;
 DROP INDEX if exists idx_exp_region;
 DROP INDEX if exists idx_imp_region;
---
+++ SQL-files/silvia.sql
@@ -0,0 +1,35 @@
+--additional materialized view
+
+CREATE MATERIALIZED VIEW MV_nations_regions AS
+SELECT
+    n_nationkey,
+    n_name,
+    r_name,
+    r_regionkey
+FROM NATION
+    JOIN REGION ON n_regionkey=r_regionkey
+
+--updated query 1
+SELECT
+	DATE_PART('year', orderdate) AS order_year,
+    DATE_PART('quarter', orderdate) AS order_quarter,
+    DATE_PART('month', orderdate) AS order_month,
+    Exp.n_name AS exp_nation,
+	Imp.n_name AS imp_nation,
+	Exp.r_name AS exp_region,
+	Imp.r_name AS imp_region,
+    SUM(revenue) AS total_revenue,
+    ptype
+FROM
+    mv_q1
+JOIN MV_nations_regions AS Exp ON Exp.n_nationkey=export_nation
+JOIN MV_nations_regions AS Imp ON Imp.n_nationkey=import_nation
+
+WHERE
+    Exp.n_name = 'FRANCE'
+    AND ptype = 'SMALL POLISHED TIN'
+GROUP BY
+    ROLLUP(ptype),
+    ROLLUP(exp_region, exp_nation),
+    ROLLUP(imp_region, imp_nation),
+    ROLLUP(order_year, order_quarter, order_month)
